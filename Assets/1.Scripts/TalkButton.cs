035c29da3a22e3ffa088455df7854f6060babd45
JongHyuk Yoon <mana951@naver.com>
2023-11-30 12:35:43 +0900

    new branch

---
+++ Assets/1.Scripts/TalkButton.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkButton : MonoBehaviour
+{
+
+    public GameObject MainCamera;
+    public GameObject TalkCamera;
+    public GameObject Button1;
+    public GameObject Button2;
+    public GameObject Talk;
+    public GameObject TalkBox;
+    public GameObject TalkBnt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ButtonDown()
+    {
+        MainCamera.SetActive(false);
+        TalkCamera.SetActive(true);
+        Button1.SetActive(false);
+        Button2.SetActive(false);
+        Talk.SetActive(false);
+        TalkBox.SetActive(true);
+        TalkBnt.SetActive(true);
+    }
+
+
+    public void TalkButton_Down()
+    {
+        MainCamera.SetActive(true);
+        TalkCamera.SetActive(false);
+        Button1.SetActive(true);
+        Button2.SetActive(true);
+        Talk.SetActive(true);
+        TalkBox.SetActive(false);
+        TalkBnt.SetActive(false);
+    }
+
+
+}
